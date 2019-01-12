36e6e0e48e13de2edc6b921f1f534d6c34d6b8ba
Andrew H <aho338@gmail.com>
2019-01-12 12:21:32 -0800

    Make config.example.js more useful

---
+++ server/config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  NEWS_API_KEY: '',
+  DARK_SKY_API_KEY: '',
+  LONG_LAT: '40.730610,-73.935242',
+  NEWS_SOURCES: 'abc-news,the-new-york-times',
+  COMMA_SEPARATED_TICKERS: 'goog,aapl'
+};
